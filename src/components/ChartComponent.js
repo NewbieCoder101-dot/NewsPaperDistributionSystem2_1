732846ae4571acec90ffd36c333737a8c22e9fae
NewbieCoder101-dot <sameeranantnag@gmail.com>
2025-01-14 13:04:23 +0530

    more changes like homepage linking in package.json etc

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,38 +1,22 @@
 // File: src/components/ChartComponent.js
 
 import React from "react";
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import "./../styles/ChartComponent.css";
-
-// Register Chart.js components
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import "../styles/ChartComponent.css";
 
 function ChartComponent({ data }) {
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "Daily Newspaper Production Stats",
-      },
-    },
-  };
-
   return (
     <div className="chart">
-      <Bar data={data} options={options} />
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="production" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
